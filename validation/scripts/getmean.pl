8ecab3e02c437be28d285d429d3c8777665e84ff
David Wilkins <david@wilkox.org>
2012-10-21 15:39:58 +1100

    Updated getmean to process false taxa removed

--- validation/scripts/getmean.pl
+++ validation/scripts/getmean.pl
@@ -3,20 +3,22 @@
 my %fp;
 my %fn;
 my %fnm;
+my %ftr;
 my @all;
-print "assemblage/taxa\tnumber of reads\tfalse positive\tfalse negative\tminspec false negative";
+print "assemblage/taxa\tnumber of reads\tfalse positive\tfalse negative\tminspec false negative\tfalse taxa removed";
 while (my $line = <STDIN>) {
   next if $. == 1;
   chomp $line;
-  die unless $line =~ /^(\d+\t\d+\t\d+)\t([\d|\.]+)\t([\d|\.]+)\t([\d|\.]+)$/;
+  die unless $line =~ /^(\d+\t\d+\t\d+)\t([\d|\.]+)\t([\d|\.]+)\t([\d|\.]+)\t([\d|\.]+)$/;
   push(@all, $1) unless exists $fp{$1};
   $fp{$1} += $2 / 5;
   $fn{$1} += $3 / 5;
   $fnm{$1} += $4 / 5;
+  $ftr{$1} += $5 / 5;
 }
 
 foreach my $line (@all) {
   die unless $line =~ /^(\d+)\t(\d+)\t(\d+)/;
   my $ratio = $2 / $1;
-  print "\n$ratio\t$3\t$fp{$line}\t$fn{$line}\t$fnm{$line}";
+  print "\n$ratio\t$3\t$fp{$line}\t$fn{$line}\t$fnm{$line}\t$ftr{$line}";
 }
