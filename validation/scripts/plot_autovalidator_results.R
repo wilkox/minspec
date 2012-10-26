009fb5f8f3fb78f708e6d1281dd4fad219e12dc9
David Wilkins <david@wilkox.org>
2012-10-26 17:31:21 +1100

    Updated axis labels on plots

--- validation/scripts/plot_autovalidator_results.R
+++ validation/scripts/plot_autovalidator_results.R
@@ -10,21 +10,21 @@ options(scipen=3)
 
 #plot false positives
 pdf("../plots/falsepositive.pdf")
-s3d = scatterplot3d(number.of.reads, assemblage.taxa, false.positive, pch=16, highlight.3d=TRUE, type="h", main="False Positive", xlab = "Number of reads", ylab = "Assemblage / All Taxa", zlab = "False Positive Rate")
+s3d = scatterplot3d(number.of.reads, assemblage.taxa, false.positive, pch=16, highlight.3d=TRUE, type="h", main="False Positive", xlab = "Number of reads", ylab = "Assemblage / All Taxa", zlab = "False Positive Rate (%)")
 fit = lm(false.positive ~ number.of.reads+assemblage.taxa)
 s3d$plane3d(fit, lty.box = "solid")
 dev.off()
 
 #plot false negatives
 pdf("../plots/falsenegative.pdf")
-s3d = scatterplot3d(number.of.reads, assemblage.taxa, false.negative, pch=16, highlight.3d=TRUE, type="h", main="False Negative", xlab = "Number of reads", ylab = "Assemblage / All Taxa", zlab = "False Negative Rate")
+s3d = scatterplot3d(number.of.reads, assemblage.taxa, false.negative, pch=16, highlight.3d=TRUE, type="h", main="False Negative", xlab = "Number of reads", ylab = "Assemblage / All Taxa", zlab = "False Negative Rate (%)")
 fit = lm(false.negative ~ number.of.reads+assemblage.taxa)
 s3d$plane3d(fit, lty.box = "solid")
 dev.off()
 
 #plot minspec false negatives
 pdf("../plots/minspecfalsenegative.pdf")
-s3d = scatterplot3d(number.of.reads, assemblage.taxa, minspec.false.negative, pch=16, highlight.3d=TRUE, type="h", main="False Negative (minspec)", xlab = "Number of reads", ylab = "Assemblage / All Taxa", zlab = "False Negative Rate (minspec)")
+s3d = scatterplot3d(number.of.reads, assemblage.taxa, minspec.false.negative, pch=16, highlight.3d=TRUE, type="h", main="False Negative (minspec)", xlab = "Number of reads", ylab = "Assemblage / All Taxa", zlab = "False Negative Rate (minspec) (%)")
 fit = lm(minspec.false.negative ~ number.of.reads+assemblage.taxa)
 s3d$plane3d(fit, lty.box = "solid")
 dev.off()
