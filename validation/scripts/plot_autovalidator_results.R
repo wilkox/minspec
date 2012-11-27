3343b97a88288247f73a582312ddc93a146d80a3
David Wilkins <david@wilkox.org>
2012-11-27 11:55:48 +1100

    "Taxa" => "OTUs"

--- validation/scripts/plot_autovalidator_results.R
+++ validation/scripts/plot_autovalidator_results.R
@@ -10,28 +10,28 @@ options(scipen=3)
 
 #plot false positives
 pdf("../plots/falsepositive.pdf")
-s3d = scatterplot3d(number.of.reads, assemblage.taxa, false.positive, pch=16, highlight.3d=TRUE, type="h", main="False Positive", xlab = "Number of reads", ylab = "Assemblage / All Taxa", zlab = "False Positive Rate (%)")
+s3d = scatterplot3d(number.of.reads, assemblage.taxa, false.positive, pch=16, highlight.3d=TRUE, type="h", main="False Positive", xlab = "Number of reads", ylab = "Assemblage / All OTUs", zlab = "False Positive Rate (%)")
 fit = lm(false.positive ~ number.of.reads+assemblage.taxa)
 s3d$plane3d(fit, lty.box = "solid")
 dev.off()
 
 #plot false negatives
 pdf("../plots/falsenegative.pdf")
-s3d = scatterplot3d(number.of.reads, assemblage.taxa, false.negative, pch=16, highlight.3d=TRUE, type="h", main="False Negative", xlab = "Number of reads", ylab = "Assemblage / All Taxa", zlab = "False Negative Rate (%)")
+s3d = scatterplot3d(number.of.reads, assemblage.taxa, false.negative, pch=16, highlight.3d=TRUE, type="h", main="False Negative", xlab = "Number of reads", ylab = "Assemblage / All OTUs", zlab = "False Negative Rate (%)")
 fit = lm(false.negative ~ number.of.reads+assemblage.taxa)
 s3d$plane3d(fit, lty.box = "solid")
 dev.off()
 
 #plot minspec false negatives
 pdf("../plots/minspecfalsenegative.pdf")
-s3d = scatterplot3d(number.of.reads, assemblage.taxa, minspec.false.negative, pch=16, highlight.3d=TRUE, type="h", main="False Negative (minspec)", xlab = "Number of reads", ylab = "Assemblage / All Taxa", zlab = "False Negative Rate (minspec) (%)")
+s3d = scatterplot3d(number.of.reads, assemblage.taxa, minspec.false.negative, pch=16, highlight.3d=TRUE, type="h", main="False Negative (minspec)", xlab = "Number of reads", ylab = "Assemblage / All OTUs", zlab = "False Negative Rate (minspec) (%)")
 fit = lm(minspec.false.negative ~ number.of.reads+assemblage.taxa)
 s3d$plane3d(fit, lty.box = "solid")
 dev.off()
 
-#plot false taxa removed
-pdf("../plots/falsetaxaremoved.pdf")
-s3d = scatterplot3d(number.of.reads, assemblage.taxa, false.taxa.removed, pch=16, highlight.3d=TRUE, type="h", main="False Taxa Removed", xlab = "Number of reads", ylab = "Assemblage / All Taxa", zlab = "False Taxa Removed")
+#plot false OTUs removed
+pdf("../plots/falseotusremoved.pdf")
+s3d = scatterplot3d(number.of.reads, assemblage.taxa, false.taxa.removed, pch=16, highlight.3d=TRUE, type="h", main="False OTUs Removed", xlab = "Number of reads", ylab = "Assemblage / All OTUs", zlab = "False OTUs Removed")
 fit = lm(false.taxa.removed ~ number.of.reads+assemblage.taxa)
 s3d$plane3d(fit, lty.box = "solid")
 dev.off()
